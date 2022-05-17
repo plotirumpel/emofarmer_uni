b94d21e663b25a1eca29db2f61e0b555096842ed
plotirumpel <88290647+plotirumpel@users.noreply.github.com>
2022-05-17 23:04:43 +0300

    Add files via upload

---
+++ с.py
@@ -0,0 +1,14 @@
+import a_vvod
+print(a_vvod.task1(200))
+
+import b_eat as b_module
+print(b_module.eat(11, 33, 55, 10, 77, 99))
+
+from a_vvod import task3
+print(task3(5))
+
+from b_eat import eat as bon_appetit
+print(bon_appetit(1, 33, 555, 7777))
+
+
+
