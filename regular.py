e99e35c15d02722e333eb2cceeb1e9c0dd9b7a68
plotirumpel <88290647+plotirumpel@users.noreply.github.com>
2022-05-27 19:49:58 +0300

    Update regular.py

--- regular.py
+++ regular.py
@@ -27,7 +27,7 @@ def regular_5(st):
     return len(cats.findall(st))
 
 def regular_6(st):
-    chislo = r'-\d+'
+    chislo = r'-?\d+'
     for m in re.findall(chislo,st):
         changed = int(m)+1
         st = re.sub(m, str(changed), st, 1)
